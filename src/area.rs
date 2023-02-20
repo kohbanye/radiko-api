e2c7dacea0d0978ef0841d69e6372a0ff3d586dc
koh <kohbanye@gmail.com>
2023-02-20 18:59:58 +0900

    add function to get area_id

---
+++ src/area.rs
@@ -0,0 +1,18 @@
+const AREA_URL: &str = "https://radiko.jp/area";
+
+pub async fn get_area() -> Result<String, Box<dyn std::error::Error>> {
+    let res = reqwest::get(AREA_URL).await.expect("failed to get area");
+
+    let body = res.text().await.expect("failed to get area");
+
+    let re = regex::Regex::new(r#""(.*?)""#).unwrap();
+    let area_id = re
+        .captures(&body)
+        .unwrap()
+        .get(1)
+        .unwrap()
+        .as_str()
+        .to_string();
+
+    Ok(area_id)
+}
--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,34 @@
 use crate::auth;
+use crate::area;
 
 #[derive(Default)]
 pub struct Client {
     pub client: reqwest::Client,
     pub auth_token: String,
+    pub area_id: String,
 }
 
+pub const V2_API_URL: &str = "https://radiko.jp/v2/api/";
+pub const V3_API_URL: &str = "https://radiko.jp/v3/api/";
+
 impl Client {
+    pub fn request(&self, method: reqwest::Method, url_path: &str) -> reqwest::RequestBuilder {
+        if self.auth_token.is_empty() {
+            panic!("auth_token is empty");
+        }
+
+        self.client
+            .request(method, url_path)
+            .header("X-Radiko-AuthToken", &self.auth_token)
+    }
+
     pub async fn auth(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         self.auth_token = auth::get_auth_token(self).await?;
+        Ok(())
+    }
+
+    pub async fn set_area_id(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.area_id = area::get_area().await?;
         Ok(())
     }
 }
